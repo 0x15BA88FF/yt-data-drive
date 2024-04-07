a654fdd04961b51b35756e61679ba1985dca8df5
Pascall de creator <pascall.de.creator@gmail.com>
2024-04-07 03:42:37 +0000

    Create Decoder Base Plate

---
+++ src/decoder.rs
@@ -0,0 +1,22 @@
+fn img_to_pixels() {
+
+}
+
+fn pixels_to_bits() {
+
+}
+
+fn split_segments(Vec<u8>) (&str, &Vec<u8>) {
+    let mut byts_buffer: Vec<u8> = Vec::new();
+}
+fn bits_to_file() {
+
+}
+
+fn clear_folder() {
+
+}
+
+pub fn main() {
+
+}
