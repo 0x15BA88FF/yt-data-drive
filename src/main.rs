465edf0d094202b9101e0bf802d7dba6f2f6a98f
Pascall de creator <pascall.de.creator@gmail.com>
2024-04-15 05:38:13 +0000

    Improve cli arguments

--- src/main.rs
+++ src/main.rs
@@ -2,24 +2,20 @@ mod encoder;
 
 use clap::Parser;
 
-#[derive(Parser, Debug)]
-#[command(version, about, long_about = None)]
+#[derive(Parser)]
+#[command(version, about = "Rust image encoder with resolution and output options", long_about = None)]
 struct Args {
-    #[arg(short, long)]
     file: String,
 
-    #[arg(short, long)]
-    output: String,
+    #[arg(short, long, help = "Output file (defaults to output.png)")]
+    output: Option<String>,
+
 }
 
 fn main() {
     let args = Args::parse();
+    let resolution: ((u16, u16), (u16, u16), (u16, u16)) = ( (3840, 2160), (1920, 1080), (1280, 720), );
+    let output_file: String = args.output.unwrap_or_else(|| "output.png".to_string());
 
-    const RESOLUTIONS: ((u16, u16), (u16, u16), (u16, u16)) = (
-        (3840, 2160),
-        (1920, 1080),
-        (1280, 720), 
-    );
-
-    encoder::run(&args.file, &args.output, RESOLUTIONS.1)
+    encoder::run(&args.file, &output_file, resolution.1)
 }
