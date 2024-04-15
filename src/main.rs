ca9b84738acbe4b69d12704da8040febcbedb80b
Pascall de creator <pascall.de.creator@gmail.com>
2024-04-15 05:45:25 +0000

    Add resolution enum

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 mod encoder;
 
-use clap::Parser;
+use clap::{Parser, ValueEnum};
 
 #[derive(Parser)]
 #[command(version, about = "Rust image encoder with resolution and output options", long_about = None)]
@@ -10,12 +10,31 @@ struct Args {
     #[arg(short, long, help = "Output file (defaults to output.png)")]
     output: Option<String>,
 
+    #[arg(short, long, help = "Output resolution (defaults to 1920x1080)")]
+    resolution: Resolution,
+}
+
+#[derive(Clone, Debug, PartialEq, ValueEnum)]
+enum Resolution {
+    Fhd,
+    Hd, // This is now the default
+    Sd,
+}
+
+impl Resolution {
+    fn to_dimensions(&self) -> (u16, u16) {
+        match self {
+            Resolution::Fhd => (3840, 2160),
+            Resolution::Hd => (1920, 1080), // Default resolution
+            Resolution::Sd => (1280, 720),
+        }
+    }
 }
 
 fn main() {
     let args = Args::parse();
-    let resolution: ((u16, u16), (u16, u16), (u16, u16)) = ( (3840, 2160), (1920, 1080), (1280, 720), );
     let output_file: String = args.output.unwrap_or_else(|| "output.png".to_string());
+    let (width, height) = args.resolution.to_dimensions();
 
-    encoder::run(&args.file, &output_file, resolution.1)
+    encoder::run(&args.file, &output_file, (width, height))
 }
